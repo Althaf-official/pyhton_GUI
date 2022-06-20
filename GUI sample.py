244a840e6aa8af964effe2afc5471f9e13fc7301
althaf <althaf.sharafudheen@outlook.com>
2022-06-20 07:08:18 +0400

    function created

--- GUI sample.py
+++ GUI sample.py
@@ -1,8 +1,17 @@
 from tkinter import * #this star for we are using to use all module from this library
 
 window=Tk()
+window.geometry("500x500")
+window.title("mas solutions")
+window.configure(background="green")
+
+#function for button click
+def hello():
+    print("button clicked")
+
+
 #create button and label
-button=Button(text="ok",width=10,height=10)
+button=Button(text="ok",command=hello)
 label=Label(window,text="welcome")
 
 #show button and label in the window
